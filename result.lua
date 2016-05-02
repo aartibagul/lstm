5c853829696a98f341e811ed840a2041cefd6edc
Aarti Bagul <aartibagul95@gmail.com>
2016-05-02 16:42:08 -0400

    Fixed results

--- result.lua
+++ result.lua
@@ -3,8 +3,8 @@ require('base')
 require 'xlua'
 ptb = require('data')
 
-predictions = io.open('testing_medium' , 'w')
-
+local batch_size = 20
+local layers = 2
 
 function reset_state(state)
     state.pos = 1
@@ -23,24 +23,24 @@ function run_test()
     
     -- no batching here
     g_replace_table(model.s[0], model.start_s)
-    for i = 1, (len - 1) do
+    for i = 1, 100 do -- (len - 1) do
         
-	xlua.progress(i, len-1)
+	xlua.progress(i, 100)
+	--xlua.progress(i, len-1)
 	local x = state_test.data[i]
         local y = state_test.data[i + 1]
         perp_tmp, model.s[1] = unpack(model.rnns[1]:forward({x, y, model.s[0]}))
-        perp = perp + perp_tmp[1]
+	perp = perp + perp_tmp[1]
         g_replace_table(model.s[0], model.s[1])
     end
-    print("Test set perplexity : " .. g_f3(torch.exp(perp / (len - 1))))
-    predictions:write("Test set perplexity : " .. g_f3(torch.exp(perp / (len - 1))))
+    print("Test set perplexity : " .. g_f3(torch.exp(perp / 100)))--(len - 1))))
     g_enable_dropout(model.rnns)
 end
 
-batch_size = 20
-layers = 2
+
+state_train = {data=ptb.traindataset(batch_size)}
+state_valid =  {data=ptb.validdataset(batch_size)}
 state_test =  {data=ptb.testdataset(batch_size)}
-reset_state(state_test)
 
 print("==> Loading model")
 model = torch.load('models/model_medium.net')
